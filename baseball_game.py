0665d9247d938def4873a95a2dc7ae032f9d7ac3
bcc0830 <bcc0830@naver.com>
2021-01-19 20:09:23 +0900

    2021.01.19

--- baseball_game.py
+++ baseball_game.py
@@ -242,9 +242,7 @@ def is_no(one_more_input):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
     return one_more_input.upper() in ['N', 'NO']
-
 
 def main():
     print("Play Baseball")
@@ -253,7 +251,22 @@ def main():
     print("Random Number is : ", random_number)
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
-
+    while True:
+        user_input = input('Input guess number : ')
+        if not is_validated_number(user_input):
+            print('Wrong Input, Input Again')
+        else:
+            res = get_strikes_or_ball(user_input, random_number)
+            print('Strikes : %d , Balls : %d' % (res[0], res[1]))
+            if res[0] == 3:
+                retry = input('You Win, one more(Y/N)?')
+                if is_yes(retry):
+                    random_number = str(get_not_duplicated_three_digit_number())
+                    print("Random Number is : ", random_number)
+                elif is_no(retry):
+                    break
+                else:
+                    print('Wrong Input, Input Again')
 
     # ==================================
     print("Thank you for using this program")
